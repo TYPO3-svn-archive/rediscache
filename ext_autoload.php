1c086bf2c7d5ecf1b8ec0d5cebb123067db36bde
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-18 13:04:59 +0000

    Fixed ext_autoload to lowercase classname
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rediscache/trunk@35776 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,7 +1,7 @@
 <?php
 $extensionClassesPath = t3lib_extMgm::extPath('rediscache') . 'Classes/';
 return array(
-        'tx_rediscache_cache_backend_RedisBackend' => $extensionClassesPath . 'class.tx_rediscache_cache_backend_RedisBackend.php'
+	'tx_rediscache_cache_backend_redisbackend' => $extensionClassesPath . 'class.tx_rediscache_cache_backend_RedisBackend.php',
 );
 
 ?>
